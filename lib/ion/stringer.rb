b7a8adc161159220016d74e697ab4a3ff34b20a5
Rico Sta. Cruz <rico@ambiescent.com>
2011-02-13 22:52:47 +0800

    Change the keyword splitting method to handle Unicode strings correctly.

--- lib/ion/stringer.rb
+++ lib/ion/stringer.rb
@@ -7,9 +7,10 @@ module Ion::Stringer
   # "Hey,  yes you." => %w(hey yes you)
   def self.keywords(str)
     return Array.new  unless str.is_a?(String)
-    self.sanitize(str).scan(/\w+/)
+    self.sanitize(str).scan(/[[:word:]]+/)  # \w doesn't work in Unicode; may not work in Ruby 1.8
   end
 
+  # "one_sign" => "OneSign"
   def self.classify(name)
     str = name.to_s
     str.scan(/\b(\w)(\w*)/).map { |(w, ord)| w.upcase + ord.downcase }.join('')
